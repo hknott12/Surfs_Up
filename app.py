622dc9b3aaf357436f7817317db90ebd6c5b9d7b
hknott12 <74023017+hknott12@users.noreply.github.com>
2021-04-08 10:37:27 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#Access the database
+engine = create_engine("sqlite:///hawaii.sqlite")
+#Reflect database
+Base = automap_base()
+#Reflect tables
+Base.prepare(engine, reflect=True)
+#Save references
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+#Create session link
+session = Session(engine)
+
+#Set Up Flask
+#define app
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
